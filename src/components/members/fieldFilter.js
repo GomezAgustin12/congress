4b46fe2da3df02d8fe61da56f2ef32ea69aa2d6b
Agustin <agustin.gomez@devlights.com>
2020-08-02 20:20:45 -0300

    Im pushing the build folder

--- src/components/members/fieldFilter.js
+++ src/components/members/fieldFilter.js
@@ -3,10 +3,8 @@ import { useSelector } from "react-redux";
 import {
   TextField,
   Container,
-  Divider,
   Select,
   MenuItem,
-  FormControlLabel,
   InputLabel,
   FormControl,
 } from "@material-ui/core";
